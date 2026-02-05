1efc14511001ad62af9052aefee51d3e78e99643
Muhammad Sameer <bhatooksameer@gmail.com>
2026-02-05 23:55:11 +0500

    Add files via upload
    31 34

---
+++ app.js
@@ -0,0 +1,54 @@
+// Ans no 1
+// var date = new Date()
+// document.writeln(date)
+
+//ans no 2
+// var montharry= ["January","Feburary","March","April","May","June" , "July","August","September","Octuber","November" , "December"]
+// var months = new Date().getMonth()
+// alert("CURRENT MONTH  : "+ montharry[months])
+
+//ans no 3
+// var months = new Date().toString()
+// var mon = months.slice(4,7)
+// alert("CURRENT MONTH  : "+ mon)
+
+//ans no 4
+// var day = new Date().getDay()
+// if(day === 6|| day === 0){
+//     alert('Today Is Funday')
+// }
+// else{
+//     alert('Today Is Work Day')
+// }
+
+//ans no 5
+// var date = new Date().getDate()
+// if(date >= 16){
+//     alert('It Is Last Fifteen Days Of Month')
+// }
+// else{
+//     alert('It Is First Fifteen Days Of Month')
+
+// }
+
+// ans no 6
+
+//ans no 7
+// var time = new Date().getHours()
+// if(time >= 12){
+//     document.writeln(`<h1>It's PM</h1>`)
+// }
+// else{
+//     document.writeln(`<h1>It's AM</h1>`)
+// }
+
+//ans no 8
+// var laterDate = new Date(2020, 11, 31);
+// document.writeln(laterDate)
+
+//ans no 9
+// var ramadan = new Date('18 June, 2015').getTime()
+// var todaystime = new Date().getTime()
+// var days = todaystime - ramadan
+// var form = days/(1000*60*60*24)
+// document.writeln(form)
