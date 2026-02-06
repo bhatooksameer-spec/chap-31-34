a333592dd31831950cbdcd96ef41f667823e78fd
Muhammad Sameer <bhatooksameer@gmail.com>
2026-02-06 11:14:16 +0500

    Refactor and uncomment JavaScript functions35 to 38

--- app.js
+++ app.js
@@ -1,54 +1,132 @@
-// Ans no 1
-// var date = new Date()
-// document.writeln(date)
+//ans no 1
+// function date(){
+//     document.writeln(new Date())
+// }
+// date()
 
 //ans no 2
-// var montharry= ["January","Feburary","March","April","May","June" , "July","August","September","Octuber","November" , "December"]
-// var months = new Date().getMonth()
-// alert("CURRENT MONTH  : "+ montharry[months])
+// function username(){
+//     var firstName = prompt("Enter Your First name").toUpperCase()
+//     var lastName = prompt("Enter Your Last name").toUpperCase()
+//     var greet =  firstName + ' ' + lastName
+//     alert("Welcome : "+greet)
+// }
+// username()
 
 //ans no 3
-// var months = new Date().toString()
-// var mon = months.slice(4,7)
-// alert("CURRENT MONTH  : "+ mon)
+// function add(){
+//     var num1 = +prompt("Enter Value")
+//     var num2 = +prompt("Enter second Value")
+//     var addition = num1 + num2
+//     document.writeln(addition)
+// }
+// add()
 
 //ans no 4
-// var day = new Date().getDay()
-// if(day === 6|| day === 0){
-//     alert('Today Is Funday')
+// function calculator(){
+//     var num1 = +prompt("Enter Value")
+//     var num2 = +prompt("Enter second Value")
+//     var op = prompt('Enter operator')
+//     if(op == '+'){
+//         document.writeln(`
+//             <h2>Your First Value Is : '${num1}'<br>Your Second Value Is : '${num2}'<br>Your Selected Operator Is : '${op}'<br>Your Addition Result Is : '${num1 + num2}'</h2>
+//             `)
+//     }
+//     else if(op == '-'){
+//         document.writeln(`
+//             <h2>Your First Value Is : '${num1}'<br>Your Second Value Is : '${num2}'<br>Your Selected Operator Is : '${op}'<br>Your Subreaction Result Is : '${num1 - num2}<'/h2>
+//             `)
+//     }
+//     else if(op == '*'){
+//         document.writeln(`
+//             <h2>Your First Value Is : '${num1}'<br>Your Second Value Is : '${num2}'<br>Your Selected Operator Is : '${op}'<br>Your Multiplication Result Is : '${num1 * num2}'</h2>
+//             `)
+//     }
+//     else if(op == '/'){
+//         document.writeln(`
+//             <h2>Your First Value Is : ${num1}'<br>Your Second Value Is : '${num2}'<br>Your Selected Operator Is : '${op}'<br>Your Division Result Is : '${num1 / num2}'</h2>
+//             `)
+//     }
+//     else if(op == '%'){
+//         document.writeln(`
+//             <h2>Your First Value Is : '${num1}'<br>Your Second Value Is : '${num2}'<br>Your Selected Operator Is : '${op}'<br>Your Modulus Result OR Reminder Is : '${num1 % num2}'</h2>
+//             `)
+//     }
+//     else (
+//         alert("This Operator Is Incorrect ! Please enter correct operator")
+//     )
 // }
-// else{
-//     alert('Today Is Work Day')
-// }
+// calculator()
 
 //ans no 5
-// var date = new Date().getDate()
-// if(date >= 16){
-//     alert('It Is Last Fifteen Days Of Month')
+// function Square(){
+// var sqvalue = parseInt(prompt('Enter Value'));
+// var square = sqvalue*sqvalue;
+//     document.writeln(`Your Square Of Given Value Is ${square}`)
 // }
-// else{
-//     alert('It Is First Fifteen Days Of Month')
+// Square()
 
+//ans no 6
+// function factorial(){
+// var num1 = +prompt('Enter value')
+// var result = 1 ;
+// for(var i=1;i<=num1;i++){
+//     result *= i;
 // }
-
-// ans no 6
+// document.writeln(result)    
+// }
+// factorial()
 
 //ans no 7
-// var time = new Date().getHours()
-// if(time >= 12){
-//     document.writeln(`<h1>It's PM</h1>`)
+// function counting(){
+// var num1 = +prompt('Enter start value')
+// var num2 = +prompt('Enter end value')
+// for(var i=num1;i<=num2;i++){
+//     document.writeln(i+"<br>")
 // }
-// else{
-//     document.writeln(`<h1>It's AM</h1>`)
 // }
+// counting()
 
 //ans no 8
-// var laterDate = new Date(2020, 11, 31);
-// document.writeln(laterDate)
+// function hypotanious(){
+//     var base = +prompt("Enter base value")
+//     var perpendicular = +prompt("Enter perpendicular value")
+//     function square(){
+//         var base2 = base*base
+//         var perpendicular2 = perpendicular*perpendicular
+//         var result = base2 + perpendicular2;
+//         document.writeln("Your Hypotanious Value Is : "+result)
+//     }
+//     square()
+// }
+// hypotanious()
 
 //ans no 9
-// var ramadan = new Date('18 June, 2015').getTime()
-// var todaystime = new Date().getTime()
-// var days = todaystime - ramadan
-// var form = days/(1000*60*60*24)
-// document.writeln(form)
+// function areaOfRectangle(width , height){
+    // return width* height
+// }
+// var w =+prompt("Enter value of width") 
+// var h =+prompt("Enter value of height") 
+// var getValue = areaOfRectangle(+prompt('Enter value of width') ,+prompt("Enter value of height"))
+// var getValue = areaOfRectangle(w , h)
+// document.writeln(getValue)
+
+//ans no 10
+// function palindrome() {
+//     var word = prompt("Enter any word to check palindrome");
+//     var check = "";
+
+//     for (var i = word.length - 1; i >= 0; i--) {
+//         check += word[i];
+//     }
+
+//     if (check === word) {
+//         document.writeln("<h2>WORD IS PALINDROME</h2>");
+//     } else {
+//         document.writeln("<h2>WORD IS NOT PALINDROME</h2>");
+//     }
+// }
+
+// palindrome();
+
+//ans no 11
